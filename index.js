db54deabdebbff8adbb8f73711dc45f4dbfe93d0
leek <lsj8380880@hotmail.com>
2020-01-03 10:52:29 +0800

    by leek

--- index.js
+++ index.js
@@ -11,26 +11,33 @@ var path = require('path');
 var base_path = '/www/web'
 
 readdir(base_path, function (base_files) {
-    console.log(base_files)
+
 
     base_files.forEach(function (item) {
         let base_files_path = path.join(base_path, item)
+        console.log(base_files_path)
 
         var base_files_path_stats = getFsStats(base_files_path)
+        console.log(base_files_path_stats)
 
         if (!isDic(base_files_path_stats)) {
+            console.log('not is dic')
             return false;
         }
 
         let artisan_path = path.join(base_files_path, 'artisan')
+        console.log(artisan_path)
 
         if (!isExists(artisan_path)) {
+            console.log('not is exists artisan')
             return false;
         }
 
         let laravel_log_path = path.join(base_files_path, 'storage/logs/laravel.log')
+        console.log(laravel_log_path)
 
         if (!isExists(laravel_log_path)) {
+            console.log('not is exists laravel.log')
             return false;
         }
 
